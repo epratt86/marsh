d855452450d2e7b917132e9de3d974271440b4a9
Eric Pratt <ericpratt86@gmail.com>
2020-02-29 14:08:44 -0800

    update site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `TA Marsh Roofing`,
+    description: `TA Marsh offers roofing services of all types and sizes. Whether you need a professional to roof, re-roof, a general contractor or even an expert witness, TA Marsh provides a weath of knowlege and experience.`,
     author: `@gatsbyjs`,
   },
   plugins: [
